e438518ddb1e2043d1474c2302a7ac694c5be5ae
jpazg11 <jpazg11@miumg.edu.gt>
2025-08-02 18:50:10 -0600

    commit base del proyecto de calculadora

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Ingrese el primer número: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Seleccione la operación:");
+        Console.WriteLine("1 - Suma");
+        Console.WriteLine("2 - Resta");
+        Console.WriteLine("3 - Multiplicación");
+        Console.WriteLine("4 - División");
+
+        Console.Write("Opción: ");
+        int opcion = Convert.ToInt32(Console.ReadLine());
+
